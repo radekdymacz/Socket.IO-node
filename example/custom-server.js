f64dfc2444354391eff5164fe70c813eae87dbc3
radekdymacz <radekdymacz@gmail.com>
2011-02-14 14:36:40 +0000

    websockets test

--- example/custom-server.js
+++ example/custom-server.js
@@ -177,7 +177,7 @@ io.on('connection', function(client){
 		}
 		if(message.action == "get" && message.recordType == "client"){
 		 //	io.clients[client.sessionId].send({clients:accounts});
-		 client.broadcast({clients:accounts,sessionid:message.sessionid});
+		 client.broadcast({clients:accounts,sessionid:message.sessionid,name:message.name});
 		 }
 
     client.broadcast(message);
